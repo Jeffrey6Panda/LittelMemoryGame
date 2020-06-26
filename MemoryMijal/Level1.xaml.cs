4d6249e9f2a6256c6da21c0e4c96443f7f5e9ec1
Gregor Mijal <Jeffrey6Panda@googlemail.com>
2020-06-26 08:20:19 +0200

    Menu navigation and randomly fill the cards

---
+++ MemoryMijal/Level1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryMijal
+{
+    /// <summary>
+    /// Interaktionslogik für Level1.xaml
+    /// </summary>
+    public partial class Level1 : Page
+    {
+        private bool multiplayer;
+        Random random = new Random();
+
+        List<string> nummbers = new List<string>()
+        {
+            "1","1","2","2","3","3","4","4","5","5","6","6",
+        };
+
+        public Level1(bool pMultiplayer)
+        {
+            InitializeComponent();
+            ButtonsGetFill();
+            multiplayer = pMultiplayer;
+        }
+
+        private void ButtonsGetFill()
+        {
+            Button button;
+            int randomNumber;
+
+            for (int i = 0; i < gridCards.Children.Count; i++)
+            {
+                if (gridCards.Children[i] is Button)
+                    button = (Button)gridCards.Children[i];
+                else
+                    continue;
+
+                randomNumber = random.Next(0, nummbers.Count);
+                button.Content = nummbers[randomNumber];
+
+                nummbers.RemoveAt(randomNumber);
+            }
+
+        }
+        private void btnInGameEnd_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new MainMenu());
+        }
+    }
+}
---
+++ MemoryMijal/SingelplayerMenu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryMijal
+{
+    /// <summary>
+    /// Interaktionslogik für SingelplayerMenu.xaml
+    /// </summary>
+    public partial class SingelplayerMenu : Page
+    {
+        public SingelplayerMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSingelBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Play());
+        }
+
+        private void btnLevel1_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Level1(false));
+        }
+    }
+}
